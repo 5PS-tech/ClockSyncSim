77df790f9775768ec3b2bf5f6022e0eefa2b406f
Sabrina Pereira <spereira@olin.edu>
2020-08-05 13:03:53 -0500

    Adding progress output

--- clocksync_funcs.py
+++ clocksync_funcs.py
@@ -31,8 +31,23 @@ def local_increment(clock_freq,timeslot_period,freq_tolerance=.5):
 # Error plot function
 def sim_plot(df, node_count):
     fig=plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-    for n0, n1 in combinations(range(node_count),2):
-        plt.scatter(df.index, df[str(n0)]-df[str(n1)]);
+
+    print("Plot Setup Commencing...")
+
+    # combs = list(combinations(range(node_count),2))
+    # print(combs)
+    #
+    #
+    combs = sum(1 for pair in combinations(range(node_count),2))
+
+    for comb, t in enumerate(combinations(range(node_count),2)):
+        n0, n1 = t
+        if (comb%(combs//4)) == 0 and comb < combs-4:
+            print("{}.0%".format(math.ceil((comb/combs)*100)), end =" - ")
+        plt.scatter(df.index, df[str(n0)]-df[str(n1)])
+
+    print("100.0%") #Plot Setup Complete...
+    print("Please wait a moment for the plot to appear.\n")
 
     plt.xlabel('Time Slots')
     plt.ylabel('Skew (ticks)')
@@ -51,13 +66,16 @@ def clock_sync_sim(freq_tolerance=.5, adjustment_func=daisy_adj, clock_freq = 40
     df.Counter=df.index*nominal_macrotick_length
 
     df['Node Reporting'] = df.index%node_count
-
     nodes_ref = list(range(node_count))
 
+    print("Simulation Setup Commencing...")
     for i in df.index:
         reporting_node = df.loc[i,'Node Reporting']
         comparison_nodes = nodes_ref.copy()
         comparison_nodes.remove(reporting_node)
+
+        if (i%(sim_length//4)) == 0 and i < sim_length-4:
+            print("{}.0%".format(math.ceil((i/sim_length)*100)), end =" - ")
 
         for j in range(node_count):
             if i == 0:
@@ -69,15 +87,16 @@ def clock_sync_sim(freq_tolerance=.5, adjustment_func=daisy_adj, clock_freq = 40
         for comparison_node in comparison_nodes:
             df.loc[i,str(comparison_node)] = df.loc[i,str(comparison_node)] + adjustment_func(df, i, reporting_node, comparison_node, r)
 
+    print("100.0%\n") #Simulation Setup Complete
 
     fmax=clock_freq*(1+freq_tolerance/100) # Max freq per error
     fmin=clock_freq*(1-freq_tolerance/100) # Min freq per error
 
     # max_dev = rho(max drift rate) * R(resync interval) * 2 * r
-    max_dev = math.ceil(abs(2*r*timeslot_period*(fmin-fmax)))
-    nominal_tick_length = (max_dev/clock_freq)*(10**6)
-
-    print("Maximum Deviation: +/- {} ticks ({} usec at nominal frequency)".format(max_dev,nominal_tick_length))
+    rho = (fmax-fmin)/clock_freq
+    max_dev = rho*timeslot_period*(10**6) # Convert to usec
+    nominal_tick_count = max_dev*clock_freq/(10**6) # Divide by unit adjustment
 
     sim_plot(df, node_count)
+    print("Maximum Deviation: +/- {:.2f} usec ({} ticks at nominal frequency)".format(max_dev,math.ceil(nominal_tick_count)))
     return df
